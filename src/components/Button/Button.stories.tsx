df169eb3aa60808938ffc6463f4767c0038057c5
Yisus5310 <kevinpoomsae2010@hotmail.com>
2021-10-03 17:43:07 -0500

    Update color and arrows

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -7,6 +7,25 @@ const ButtonStory = {
   title: 'Components/Button',
 };
 
-export const DefaultButton = () => <Button color={'red'} />;
-
+export const DefaultButton = () => 
+  <Button 
+    color={'red'}
+    width="100px"
+    height="100px"
+    //borders="40px" 
+    //rotate={'0deg'}
+    //left?: "45px"
+    //top?: "45px"
+  />;
+export const Button2 = () =>  
+  <Button 
+  color={'black'}
+  width="100px"
+  height="100px"
+  //left: "60px"
+  //top: "60px"
+  
+  //borders="80px"
+  //rotate={'180deg'}
+  />;
 export default ButtonStory;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,20 +4,49 @@ import { jsx } from 'theme-ui';
 
 interface IProps {
   color: string;
+  width?: string;
+  height?: string;
+
+  //rotate: string;
 }
 
-const Button: React.FC<IProps> = ({ color }) => (
+const Button: React.FC<IProps> = ({ 
+  color,
+  width = '10rem',
+  height = '10rem',
+  //rotate, 
+}) => (
   <div
     sx={{
-      backgroundColor: '#211111',
-      width: 100,
-      height: 100,
-      borderRadius: 10,
+      backgroundColor: color,
+      width: width,
+      height: height,
+      borderRadius: 25,
       color: 'white',
     }}
   >
-    Hello World
+    <div
+      sx={{
+        position: 'absolute',
+        backgroundColor: color,
+        width: 0,
+        height: 0,
+        borderLeft: '40px solid white',
+        borderTop: '20px solid transparent',
+        borderBottom: '20px solid transparent',
+        
+        borderRadius: '5px',
+        /**zIndex: 2,
+        **/
+        left: '45px',
+        top: '45px',
+        transform: 'rotate(90deg)'
+      }}
+    />
+  
+    
   </div>
+  
 );
 
 export default Button;
