402e6346517af6b87cbf8d2c392c743517744a97
Yisus5310 <kevinpoomsae2010@hotmail.com>
2021-10-09 20:42:50 -0500

    Create histories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -9,23 +9,46 @@ const ButtonStory = {
 
 export const DefaultButton = () => 
   <Button 
-    color={'red'}
+    color={'#3da78b'}
     width="100px"
-    height="100px"
-    //borders="40px" 
-    //rotate={'0deg'}
-    //left?: "45px"
-    //top?: "45px"
+    height="100px" 
+    direction={'RIGHT'}
+    borderLeft= "40px solid white"
+    borderTop= "20px solid transparent"
+    borderbottom= "20px solid transparent"
   />;
-export const Button2 = () =>  
+
+export const Button1 = () =>  
   <Button 
-  color={'black'}
+  color={'#241011'}
   width="100px"
   height="100px"
-  //left: "60px"
-  //top: "60px"
-  
-  //borders="80px"
-  //rotate={'180deg'}
+  direction={'UP'}
+  borderLeft= "40px solid white"
+  borderTop= "20px solid transparent"
+  borderbottom= "20px solid transparent"
   />;
+
+  export const Button2 = () =>  
+  <Button 
+  color="rgba(36, 16, 17, 1.5)"
+  width="100px"
+  height="100px"
+  direction={'DOWN'}
+  borderLeft= "40px solid white"
+  borderTop= "20px solid transparent"
+  borderbottom= "20px solid transparent"
+  />;
+
+  export const Button3 = () =>  
+  <Button 
+  color={'#241011'}
+  width="7rem"
+  height="7rem"
+  borderLeft= {'20px solid blue'}
+  borderTop= {'10px solid transparent'}
+  borderbottom= {'10px solid transparent'}
+  direction={'LEFT'}
+  />;
+
 export default ButtonStory;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,35 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-
+//creacion de tipo
+type IDirection = 'UP' | 'DOWN' | 'LEFT' | 'RIGHT';
 interface IProps {
   color: string;
   width?: string;
   height?: string;
-
-  //rotate: string;
+  direction?: IDirection;
 }
 
-const Button: React.FC<IProps> = ({ 
+const getDirection = (direction: IDirection) => {
+  switch (direction) {
+    case "UP":
+      return '270';
+    case "DOWN":
+      return '90';
+    case "LEFT":
+      return '180';
+    case "RIGHT":
+      return '0';
+    default:
+      throw new Error('No valid direction');
+  }
+}
+
+const Button: React.FC<IProps> = ({
   color,
   width = '10rem',
   height = '10rem',
-  //rotate, 
+  direction = 'RIGHT' , 
 }) => (
   <div
     sx={{
@@ -34,19 +49,19 @@ const Button: React.FC<IProps> = ({
         borderLeft: '40px solid white',
         borderTop: '20px solid transparent',
         borderBottom: '20px solid transparent',
-        
+
         borderRadius: '5px',
         /**zIndex: 2,
         **/
         left: '45px',
         top: '45px',
-        transform: 'rotate(90deg)'
+        transform: `rotate(${getDirection(direction)}deg)`,
       }}
     />
-  
-    
+
+
   </div>
-  
+
 );
 
 export default Button;
